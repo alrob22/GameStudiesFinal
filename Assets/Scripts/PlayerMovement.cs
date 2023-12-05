6f75371a6fd7e9a6c6955302d9b650a5d92a9a54
Aden Robertson <alrob2224@gmail.com>
2023-12-05 12:16:28 -0500

    added new sprites and collision map

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,12 +7,39 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 5f;
     public Rigidbody2D rb;
     Vector2 movement;
+    private SpriteRenderer spriteRenderer;
+    public Sprite down;
+    public Sprite up;
+    public Sprite right;
+    public Sprite left;
+
+    void Start()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
 
     // Update is called once per frame
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKeyDown("down")) {
+            Debug.Log("down was pressed");
+            spriteRenderer.sprite = down;
+        }
+        if (Input.GetKeyDown("up")) {
+            Debug.Log("up was pressed");
+            spriteRenderer.sprite = up;
+        }
+        if (Input.GetKeyDown("right")) {
+            Debug.Log("right was pressed");
+            spriteRenderer.sprite = right;
+        }
+        if (Input.GetKeyDown("left")) {
+            Debug.Log("left was pressed");
+            spriteRenderer.sprite = left;
+        }
 
         // boundary checks for the screen
         // left
@@ -35,5 +62,25 @@ public class PlayerMovement : MonoBehaviour
     
     void FixedUpdate() {
         rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("collisionMap")) {
+            Debug.Log("collision map hit");
+
+            if (Input.GetKeyDown("down")) {
+                movement.y--;
+            }
+            if (Input.GetKeyDown("up")) {
+                movement.y++;
+            }
+            if (Input.GetKeyDown("right")) {
+                movement.x--;
+            }
+            if (Input.GetKeyDown("left")) {
+                movement.x++;
+            }
+        }
     }
 }
